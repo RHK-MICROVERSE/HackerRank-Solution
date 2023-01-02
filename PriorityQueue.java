6742e9e33af69fb4dbbe506e2db7ae0f2b491d71
Rassel Hassan Kadir <9rhktrading@gmail.com>
2023-01-02 15:25:02 +0600

    First Commit

---
+++ PriorityQueue.java
@@ -0,0 +1,18 @@
+
+
+import java.util.PriorityQueue;
+
+
+
+public class PriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue pr = new PriorityQueue();
+        pr.add("Kiwi");
+        pr.add("Apple");
+        pr.add("Mango");
+        pr.add("Banna");
+        
+        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+    }
+    
+}
